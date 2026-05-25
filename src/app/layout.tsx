6159103a43e530d97f4ff08b666a181435eea68c
crumbs Am <crumbsam@crumbsdeMacBook-Air.local>
2026-05-25 16:10:53 +0800

    添加网站图标资源

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,15 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: t.meta.title,
     description: t.meta.description,
+    icons: {
+      shortcut: "/favicon.ico",
+      icon: [
+        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        { url: "/icon.png", sizes: "512x512", type: "image/png" },
+      ],
+      apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+    },
   };
 }
 
